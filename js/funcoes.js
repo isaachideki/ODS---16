cb29ebcaee82b6a375b59aad6dbfa94aca196cd6
isaac hideki <hidekisaac1@gmail.com>
2022-12-08 19:54:30 -0300

    finalizado

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,62 +1,42 @@
+//Criar uma variável para manipular o XML
 xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "xml/conteudo.xml", false);
+//Seleciona o arquivo XML
+xmlhttp.open("GET","xml/conteudo.xml",false);
+//Enviar o arquivo para a leitura
 xmlhttp.send();
+//Informar que o arquivo enviado é um XML
 xmlDoc = xmlhttp.responseXML;
-x = xmlDoc.getElementsByTagName("conteudo");
-function titulo1() {
-    for (i = x.length - 1; i >= 0; i--) {
-        document.write("<h1 class='text-center mt-3'>" +
-            x[i].getElementsByTagName("titulo1")[0].childNodes[0].nodeValue +
-            "</h1>");
+
+y = xmlDoc.getElementsByTagName("postagem");
+x = xmlDoc.getElementsByTagName("noticia");
+
+function postagemPequena() {
+  for(i= y.length-1;i>=0;i--){
+    document.write(
+    "<div class='col'>" +
+      "<div class='card mt-5' style='width: 22rem;'>" +
+        "<img src='img/" + y[i].getElementsByTagName("img-blog")[0].childNodes[0].nodeValue +"' class='card-img-top' alt='...'>" +
+        "<div class='card-body'>" +
+          "<h4 class='card-title'>" +y[i].getElementsByTagName("titulo-blog")[0].childNodes[0].nodeValue + "</h4>" + "<p class='card-text'>" + y[i].getElementsByTagName("corpo-blog")[0].childNodes[0].nodeValue.substr(0,100) +"</p>" + "</div>" +
+          "<div class='card-body'>" +
+            "<a href='blog.html?codigo_noticia=" + i +"' class='card-link'>Continue lendo</a>" +
+          "</div>" +
+        "</div>" +
+      "</div>"
+    );
     }
-}
+  }
 
-function titulo2() {
-    for (i = x.length - 1; i >= 0; i--) {
-        document.write("<h1 class='text-center'>" +
-            x[i].getElementsByTagName("titulo2")[0].childNodes[0].nodeValue +
-            "</h1>");
-    }
-}
-
-
-function texto1() {
-    for (i = x.length - 1; i >= 0; i--) {
-        document.write("<h2 class='mt-5 pt-5'>" +
-            x[i].getElementsByTagName("texto1")[0].childNodes[0].nodeValue +
-            "</h2>");
-    }
-}
-
-function texto2() {
-    for (i = x.length - 1; i >= 0; i--) {
-        document.write("<h2 class='mt-5 pt-4'>" +
-            x[i].getElementsByTagName("texto2")[0].childNodes[0].nodeValue +
-            "</h2>");
-    }
-}
-
-
-function titulo3() {
-    for (i = x.length - 1; i >= 0; i--) {
-        document.write("<h1 class='pt2 text-center mt-5'>" +
-            x[i].getElementsByTagName("titulo3")[0].childNodes[0].nodeValue +
-            "</h1>");
-    }
-}
-
-function meta1() {
-    for (i = x.length - 1; i >= 0; i--) {
-        document.write("<p>" +
-            x[i].getElementsByTagName("meta1")[0].childNodes[0].nodeValue +
-            "</p>");
-    }
-}
-
-
-
-
-
-
-
-
+  function mostrarPostagem(){
+    url = new URL(window.location.href);
+    parametro = url.searchParams;
+    i = parametro.get("codigo_noticia");
+    document.write(
+    "<div>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</div>" + 
+    "<div>" + x[i].getElementsByTagName("data")[0].childNodes[0].nodeValue + "</div>" +
+    "<div class='col-auto d-none d-lg-block'>" +
+    "<img src='img/"+ x[i].getElementsByTagName("imgpostagem")[0].childNodes[0].nodeValue + "' width='200' height='250'>" + 
+    "</div>" +
+    "<div>" + x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue + "</div>"
+    )
+  }
